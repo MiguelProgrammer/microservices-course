1306e1d153602c7d4ccb2e81c70abcc01c63b6bb
Miguel Pereira da Silva <miguel.silva@avanade.com>
2022-05-10 12:58:52 -0300

    Updates on files

--- hr-payroll/src/main/java/br/com/estudandoemcasa/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/estudandoemcasa/payroll/services/PaymentService.java
@@ -1,20 +1,19 @@
 package br.com.estudandoemcasa.payroll.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 
 import br.com.estudandoemcasa.payroll.entities.Payment;
 import br.com.estudandoemcasa.payroll.entities.Worker;
-import br.com.estudandoemcasa.payroll.exception.PayMentException;
 import br.com.estudandoemcasa.payroll.feignclients.WorkerFeignClient;
-import feign.FeignException;
 
 @Service
 public class PaymentService {
 
 	@Autowired
 	private WorkerFeignClient workerFeignClient;
-
+	
 	public Payment getPayment(Long workerId, int days) {
 
 		Worker worker = workerFeignClient.findById(workerId).getBody();
